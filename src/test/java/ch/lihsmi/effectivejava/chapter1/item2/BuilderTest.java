e6feae769936e789074960b420788e84e10769c1
Michael Lihs <mimi@kaktusteam.de>
2016-07-03 21:33:55 +0200

    [TASK] Add additional tests for chapter 1 / item 1

--- src/test/java/ch/lihsmi/effectivejava/chapter1/item2/BuilderTest.java
+++ src/test/java/ch/lihsmi/effectivejava/chapter1/item2/BuilderTest.java
@@ -14,4 +14,28 @@ public class BuilderTest {
         assertEquals(null, product.getAttribute3());
     }
 
+    @Test
+    public void builderCreatesExpectedObjectIfOnlyAttribute2IsGiven() {
+        Product product = new Product.Builder("attribute1").withAttribute2("attribute2").build();
+        assertEquals("attribute1", product.getAttribute1());
+        assertEquals("attribute2", product.getAttribute2());
+        assertEquals(null, product.getAttribute3());
+    }
+
+    @Test
+    public void builderCreatesExpectedObjectIfOnlyAttribute3IsGiven() {
+        Product product = new Product.Builder("attribute1").withAttribute3("attribute3").build();
+        assertEquals("attribute1", product.getAttribute1());
+        assertEquals(null, product.getAttribute2());
+        assertEquals("attribute3", product.getAttribute3());
+    }
+
+    @Test
+    public void builderCreatesExpectedObjectIfAllAttributesAreGiven() {
+        Product product = new Product.Builder("attribute1").withAttribute2("attribute2").withAttribute3("attribute3").build();
+        assertEquals("attribute1", product.getAttribute1());
+        assertEquals("attribute2", product.getAttribute2());
+        assertEquals("attribute3", product.getAttribute3());
+    }
+
 }
